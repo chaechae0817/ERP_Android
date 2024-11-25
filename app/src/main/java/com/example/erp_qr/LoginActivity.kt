32afc1ba75db69bc389dd54c751740855ed0cec0
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2024-11-25 15:44:02 +0900

    first commit

---
+++ app/src/main/java/com/example/erp_qr/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.erp_qr
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.erp_qr.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
+
+    }
+}
