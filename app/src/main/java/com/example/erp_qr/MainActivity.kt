f5b390368665c7cbb156e424d04225a8756e2e42
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-01-18 02:23:27 +0900

    이미지

--- app/src/main/java/com/example/erp_qr/MainActivity.kt
+++ app/src/main/java/com/example/erp_qr/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
 import com.example.erp_qr.attendance.AttendanceFragment
 import com.example.erp_qr.databinding.ActivityMainBinding
 import com.example.erp_qr.databinding.NavHeaderBinding
@@ -104,19 +103,19 @@ class MainActivity : AppCompatActivity() {
             when (menuItem.itemId) {
                 R.id.nav_home -> {
                     loadFragment(MainFragment())
-                    binding.toolbar.title = "모바일 열람"
+                    binding.toolbar.title = "출입용 QR"
                 }
                 R.id.nav_vacation -> {
                     loadFragment(VacationFragment())
-                    binding.toolbar.title = "휴가 열람"
+                    binding.toolbar.title = "휴가"
                 }
                 R.id.nav_attendance -> {
                     loadFragment(AttendanceFragment())
-                    binding.toolbar.title = "근태 열람"
+                    binding.toolbar.title = "근태"
                 }
                 R.id.nav_salary -> {
                     loadFragment(SalaryFragment())
-                    binding.toolbar.title = "급여 열람"
+                    binding.toolbar.title = "급여"
                 }
             }
             true
